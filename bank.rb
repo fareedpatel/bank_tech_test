7951f65a355afb874ba57226b2c61d963a508774
fareedpatel <fareed_patel@hotmail.co.uk>
2016-04-06 15:51:27 +0100

    first passing test

--- bank.rb
+++ bank.rb
@@ -1,3 +1,12 @@
-class Bank
+class Account
+  attr_reader :transactions
 
+  def initialize(account_number, beginning_balance = 0)
+    @account_number = account_number
+    @transactions = [ beginning_balance ]
+  end
+
+  def balance
+    transactions
+  end
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,7 +1,14 @@
 require './bank'
 
-describe Bank do
-  it "displays a black users name" do
-    name.new.should == {}
+
+describe Account do
+  let(:account) {Account.new("12345678", 100)}
+
+
+  describe '#initialize' do
+    it "should be an instance of a bank balance" do
+      account.should  be_an_instance_of Account
+    end
   end
+
 end
